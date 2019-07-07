fa94ae47f04204e28db9f9c9913fc8b84c95941b
Olivia Kesler <oakesler@protonmail.ch>
2019-07-07 06:41:31 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,19 @@
 #write your code here
 
-def countdown
+def countdown(integer)
+  counter = integer
+  while counter < integer 
+    puts  "#{counter} SECOND(S)!"
+    counter -= 1
+  end
+  return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(integer)
+  counter = 0
+  while counter < integer
+    sleep 1
+    puts "#{counter} SECOND(S)!"
+    counter -= 1 
+  end
 end
