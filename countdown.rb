cc96b7e75d885bb418a64e722ba40f0476e401ea
Olivia Kesler <oakesler@protonmail.ch>
2019-07-07 06:54:26 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -13,6 +13,6 @@ def countdown_with_sleep(integer)
   while counter > 0 
     puts "#{counter} SECOND(S)!"
     counter -= 1 
-    sleep 1.seconds until counter == 0 
+    sleep 1.seconds until counter == 1
   end
 end
