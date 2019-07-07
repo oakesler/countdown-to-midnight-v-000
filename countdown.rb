ee6bef595cbfb6294160bd472acd7edf96775977
Olivia Kesler <oakesler@protonmail.ch>
2019-07-07 06:59:15 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
-#write your code here
 
-def countdown
+def countdown(integer)
+  counter = integer
+  while counter > 0 
+    puts  "#{counter} SECOND(S)!"
+    counter -= 1
+  end
+  return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(integer)
+  counter = integer
+  while counter > 0 
+    puts "#{counter} SECOND(S)!"
+    sleep 1
+    counter -= 1 
+  end
 end
